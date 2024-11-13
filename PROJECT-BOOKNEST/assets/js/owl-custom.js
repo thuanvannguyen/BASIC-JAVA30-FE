5ddfe6518c15ad2706dea699352678d03ba3040a
ThuanVan Nguyen <60531224+thuanvannguyen@users.noreply.github.com>
2024-11-13 20:47:38 +0700

    Update slider

--- PROJECT-BOOKNEST/assets/js/owl-custom.js
+++ PROJECT-BOOKNEST/assets/js/owl-custom.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
 
-
   // $(".owl-carousel").owlCarousel();
   $('.owl-carousel').owlCarousel({
-    loop: false, //Vong lap
+    loop: true, //Vong lap
     margin: 10, // k/c giua cac item
-    nav: true, // Nut dieu huong
+    nav: false, // Nut dieu huong
+    dots: true, // dau tron chuyen slide
+    autoplay: true, // Tu dong chuyen slide
+    autoplayTimeout: 1500, // ms
     responsive: {
       0: {
         items: 1
